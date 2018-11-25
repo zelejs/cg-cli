0650e1f030401cadf5e326494bef37abc0a54993
vincent@macbook <vincent@macbook.air>
2018-11-25 21:11:44 +0800

    cg_master_resource_record for onemany x2

--- assets/src/main/resources/sql/cg-mysql-schema.sql
+++ assets/src/main/resources/sql/cg-mysql-schema.sql
@@ -54,6 +54,17 @@ CREATE TABLE `cg_master_resource_item` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `cg_master_resource_record`;
+CREATE TABLE `cg_master_resource_record` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `name` varchar(100) NOT NULL COMMENT '名字',
+  `other_id` bigint(20) NOT NULL COMMENT '主体ID',
+  `master_field` varchar(100) DEFAULT NULL COMMENT '关联主体字段',
+  `title` varchar(100) DEFAULT NULL COMMENT '标题',
+  `description` varchar(200) DEFAULT NULL COMMENT '说明',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `cg_master_resource_peer`;
 CREATE TABLE `cg_master_resource_peer` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
