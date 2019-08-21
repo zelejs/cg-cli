8af93c6c0c2771fd44abafaf32318dc5a454727f
onSunday <996597885@qq.com>
2019-08-21 15:49:35 +0800

    feature: 添加swagger支持

--- assets/src/main/java/com/jfeat/AmApplication.java
+++ assets/src/main/java/com/jfeat/AmApplication.java
@@ -1,7 +1,11 @@
 package com.jfeat;
 
+import com.jfeat.am.config.properties.AmProperties;
 import org.springframework.boot.SpringApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * SpringBoot CG Test Application
@@ -10,7 +14,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2017/5/21 12:06
  */
 @SpringBootApplication
-public class AmApplication{
+@EnableSwagger2
+public class AmApplication extends WebMvcConfigurerAdapter {
+
+    @Resource
+    private AmProperties amProperties;
+
+    /**
+     * 增加swagger的支持
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if(amProperties.getSwaggerOpen()){
+            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
